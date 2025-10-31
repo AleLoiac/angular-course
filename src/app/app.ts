28a6a28b0d2b14868a91d29d8fd826067b32c39d
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-10-31 11:47:41 +0100

    Use conditional rendering and loops in templates

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,17 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('angular-course');
+  protected readonly title = signal('App Angular');
+
+  x = 10;
+
+  students = [
+    { id: 1, fn: 'Mario', ln: 'Rossi' },
+    { id: 2, fn: 'Luigi', ln: 'Verdi' },
+    { id: 3, fn: 'Laura', ln: 'Neri' },
+    { id: 4, fn: 'Anna', ln: 'Bianchi' },
+  ];
 }
