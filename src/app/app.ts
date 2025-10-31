81ae2fffd083a5a23224e85f3193411567bd5ca7
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-10-31 12:38:58 +0100

    Use signals combined with conditionals and loops

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,10 +12,42 @@ export class App {
 
   x = 10;
 
-  students = [
+  students = signal([
     { id: 1, fn: 'Mario', ln: 'Rossi' },
     { id: 2, fn: 'Luigi', ln: 'Verdi' },
     { id: 3, fn: 'Laura', ln: 'Neri' },
     { id: 4, fn: 'Anna', ln: 'Bianchi' },
-  ];
+  ]);
+
+  counter = signal(0);
+
+  increaseCounter = () => {
+    this.counter.update((value) => value + 1);
+  };
+  decreaseCounter = () => {
+    this.counter.update((value) => value - 1);
+  };
+  setCounter = (v: number) => {
+    this.counter.set(v);
+  };
+
+  addStudent = () => {
+    const id = this.students().length + 1;
+    const newStudent = {
+      id,
+      fn: `New Student FirstName` + id,
+      ln: `New Student LastName` + id,
+    };
+    this.students.update((v) => [...v, newStudent]);
+  };
+
+  updateStudentInfo = (id: number) => {
+    this.students.update((value) =>
+      value.map((student) => (student.id === id ? { ...student, ln: 'Green' } : student))
+    );
+  };
+
+  orderStudentsRandomly = () => {
+    this.students.update((value) => value.sort(() => Math.random() - 0.5));
+  };
 }
