1cfaf7eb390ce549936a7a84f109e77126f719be
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-11-01 12:25:00 +0100

    Use ngClass and ngStyle

--- src/app/box/box.ts
+++ src/app/box/box.ts
@@ -1,19 +1,27 @@
+import { NgClass, NgStyle } from '@angular/common';
 import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'box',
-  imports: [],
+  imports: [NgClass, NgStyle],
   templateUrl: './box.html',
   styles: `
-  .box {
-    background-color: lightblue;
-  }
+  .box { background-color: lightblue; }
+  .btn { padding: 10px; color: white; }
+  .btn-primary { background-color: blue; }
+  .btn-danger { background-color: red; }
   `,
 })
 export class Box {
   isBox = signal(true);
+
   bg = signal('lightgreen');
   width = signal(200);
+
+  isPrimary = signal(true);
+  isDanger = signal(false);
+
+  fontSize = 30;
 
   toggleBoxSignal() {
     this.isBox.update((v) => !v);
