e2b8ddaf02ad2ffe74c8e252691bd4b484a7ee9b
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-11-01 11:59:20 +0100

    Use dynamic styles with signals

--- src/app/box/box.ts
+++ src/app/box/box.ts
@@ -12,8 +12,18 @@ import { Component, signal } from '@angular/core';
 })
 export class Box {
   isBox = signal(true);
+  bg = signal('lightgreen');
+  width = signal(200);
 
   toggleBoxSignal() {
     this.isBox.update((v) => !v);
+  }
+
+  toggleBg() {
+    this.bg.update((v) => (v === 'lightgreen' ? 'red' : 'lightgreen'));
+  }
+
+  changeWidth() {
+    this.width.update((v) => (v === 200 ? 400 : 200));
   }
 }
