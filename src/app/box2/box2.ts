7a8a58eeb7260adee6fb18f4a7afc6a0ab2b4c16
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-11-01 16:16:06 +0100

    Create capitalize custom pipe

--- src/app/box2/box2.ts
+++ src/app/box2/box2.ts
@@ -1,13 +1,16 @@
 import { SlicePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { CapitalizePipe } from '../capitalize-pipe';
 
 @Component({
   selector: 'box2',
-  imports: [SlicePipe, UpperCasePipe],
+  imports: [SlicePipe, UpperCasePipe, CapitalizePipe],
   templateUrl: './box2.html',
   styleUrl: './box2.css',
 })
 export class Box2 {
   description =
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas veniam, temporibus minima dolor consequatur, optio commodi nihil eius deleniti in dolore rerum, reiciendis laudantium nesciunt facilis! Cumque eius amet asperiores.';
+
+  word = 'angular';
 }
